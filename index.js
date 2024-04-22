6c8d8d00129f3320064f24e079b0ca1fdc47e240
Shibasish3210Gaian <shibhashish.m@gaiansolutions.com>
2024-04-22 15:56:17 +0530

    added url in env file

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
-import { json, urlencoded } from 'express';
-import cors from 'cors';
-import cookieParser from 'cookie-parser'
+import { json, urlencoded } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 import { PORT, app } from "./app.js";
 import connectToMongoDB from "./db.js";
-import authRoute from './controllers/authController.js';
-import projectRoute from './controllers/projectController.js';
+import authRoute from "./controllers/authController.js";
+import projectRoute from "./controllers/projectController.js";
 
 connectToMongoDB();
 
 app.use(json());
 app.use(urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors({
-    // origin: "https://code-webby.vercel.app",
-    origin: "http://localhost:5173",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials: true,
-}))
-app.use('/auth', authRoute);
-app.use('/project', projectRoute);
+app.use(
+	cors({
+		origin: `${
+			process.env.Environment === "developement"
+				? process.env.devBaseURL
+				: process.env.prodBaseURL
+		}`,
+		methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+		credentials: true,
+	})
+);
+app.use("/auth", authRoute);
+app.use("/project", projectRoute);
 
-app.listen(PORT, ()=>{
-    console.log(`server listening on port ${PORT}`);
-})
+app.listen(PORT, () => {
+	console.log(`server listening on port ${PORT}`);
+});
